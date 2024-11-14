4534c89e632fda887c49b0893e6b9ae84d12c497
Juma Al Remeithi <juma_ad@hotmail.com>
2024-11-14 11:42:21 +0400

    Final

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
+//Includes might be highlighted as errors in intellisense because you might be missing the .vscode file
 #include <iostream>
 
 int main() {
+
     std::cout << "Hello, CMake in VS Code!" << std::endl;
+
     return 0;
 }
