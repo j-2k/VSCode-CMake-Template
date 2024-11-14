7279733a82041cee404030b3c47d36a03d9b192c
Juma Al Remeithi <juma_ad@hotmail.com>
2024-11-14 12:03:37 +0400

    test push

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 //Includes might be highlighted as errors in intellisense because you might be missing the .vscode file
 #include <iostream>
 
+//Read about how to use pragma when having many translation units & header files
+
 int main() {
 
     std::cout << "Hello, CMake in VS Code!" << std::endl;
-
+    
     return 0;
 }
